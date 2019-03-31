b582c6b422efae76b156e07f4910e70d27eb5435
Chris McLean <havok2905@gmail.com>
2019-03-30 21:32:01 -0400

    added madelines npc block

--- adventures/journey-to-the-temple-of-ao/chapters/npcs.jsx
+++ adventures/journey-to-the-temple-of-ao/chapters/npcs.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 
-import { Page, ScrollToMe } from "dnd-module";
+import {
+    CollapsableContainer,
+    MonsterBlock,
+    Page,
+    ScrollToMe
+} from "dnd-module";
+
+import MADELINE_CELESTE from "../constants/npcs/madeline-celeste";
 
 const Npcs = () => {
     return (
         <Page>
             <ScrollToMe scrollActionString="npcs" />
             <h1>NPCs</h1>
+            <CollapsableContainer title="Madeline Celeste: Mountain Guide">
+                <MonsterBlock {...MADELINE_CELESTE} />
+            </CollapsableContainer>
+
             <ul>
-                <li>Celeste Albina: Mountain Guide</li>
                 <li>Daniel Rudolf: New Fay Guard Captain</li>
                 <li>Estaban: Profane Soul Leader</li>
                 <li>Gabriel Katarina: Mysterious Elf Stranger</li>
