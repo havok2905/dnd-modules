a6f95d86a4408fd4e25f699a45b92dbf14cd773e
Chris McLean <havok2905@gmail.com>
2019-04-27 19:26:05 -0400

    added maps

--- adventures/silver-creek/index.jsx
+++ adventures/silver-creek/index.jsx
@@ -10,6 +10,9 @@ import { Creatures } from "./chapters/creatures.jsx";
 import { Characters } from "./chapters/characters.jsx";
 
 import HeroImage from "./img/hero.png";
+
+import ChurchOfTheDawn from "./img/church-of-the-dawn.png";
+import GreatTomahawkRiver from "./img/great-tomahawk-river.png";
 import RazorbackRidgeImage from "./img/razorback-ridge.png";
 import SaloonBasementImage from "./img/saloon-basement.png";
 import SaloonLevel1Image from "./img/saloon-level-1.png";
@@ -133,10 +136,22 @@ export const SilverCreek = () => {
                         <h2>Black Dust Road</h2>
                         <h2>Church of the Dawn</h2>
                         <h3>Temple to Lathander</h3>
+                        <PanZoom>
+                            <img
+                                src={ChurchOfTheDawn}
+                                alt="Church of the Dawn Map"
+                            />
+                        </PanZoom>
                         <h3>The Cemetary</h3>
                         <h2>MacDougel Ranch</h2>
                         <h2>Jackelfort Ranch</h2>
                         <h2>Great Tomahawk River</h2>
+                        <PanZoom>
+                            <img
+                                src={GreatTomahawkRiver}
+                                alt="Great Tomahawk River Map"
+                            />
+                        </PanZoom>
                         <h2>Ruins of Asmodeous</h2>
                         <Characters />
                         <Creatures />
