ef32d3385dd08a71a55ddd9eaa5cbc70201b65a0
Chris McLean <havok2905@gmail.com>
2019-07-13 15:34:07 -0600

    added npm

--- adventures/journey-to-the-temple-of-ao/chapters/npcs.jsx
+++ adventures/journey-to-the-temple-of-ao/chapters/npcs.jsx
@@ -2,11 +2,12 @@ import React, { Fragment } from "react";
 
 import { CollapsableContainer, MonsterBlock, ScrollToMe } from "dnd-module";
 
-import MADELINE_CELESTE from "../constants/npcs/madeline-celeste";
+import CECELIA_MARILUZE from "../constants/npcs/cecelia-mariluze";
 import DANIEL_RUDOLF from "../constants/npcs/daniel-rudolf";
 import ESTEBAN from "../constants/npcs/esteban";
 import GABRIEL_KATARINA from "../constants/npcs/gabriel-katarina";
 import KLAUS_CORDULA from "../constants/npcs/klaus-cordula";
+import MADELINE_CELESTE from "../constants/npcs/madeline-celeste";
 import SIGI_ELLI from "../constants/npcs/sigi-elli";
 
 const Npcs = () => {
@@ -14,8 +15,8 @@ const Npcs = () => {
         <Fragment>
             <ScrollToMe scrollActionString="npcs" />
             <h2>NPCs</h2>
-            <CollapsableContainer title="Madeline Celeste: Mountain Guide">
-                <MonsterBlock {...MADELINE_CELESTE} />
+            <CollapsableContainer title="Cecelia Mariluze: Blood Hunter">
+                <MonsterBlock {...CECELIA_MARILUZE} />
             </CollapsableContainer>
             <CollapsableContainer title="Daniel Rudolf: New Fay Guard Captain">
                 <MonsterBlock {...DANIEL_RUDOLF} />
@@ -28,6 +29,9 @@ const Npcs = () => {
             </CollapsableContainer>
             <CollapsableContainer title="Klaus Cordula: Waffle and Sausage Hut Owner">
                 <MonsterBlock {...KLAUS_CORDULA} />
+            </CollapsableContainer>
+            <CollapsableContainer title="Madeline Celeste: Mountain Guide">
+                <MonsterBlock {...MADELINE_CELESTE} />
             </CollapsableContainer>
             <CollapsableContainer title="Sigi Elli: Inn Keeper">
                 <MonsterBlock {...SIGI_ELLI} />
