8cd6f9eed50e518ced6be5942f47ca6ca24e5670
Chris McLean <havok2905@gmail.com>
2019-09-03 23:05:57 -0600

    fix adventure and edited intro

--- dm-binder/adventures/light-up-the-night/chapters/intro.tsx
+++ dm-binder/adventures/light-up-the-night/chapters/intro.tsx
@@ -1,31 +1,50 @@
 import React, { Fragment } from "react";
 
-export const Epilogue = () => {
+export const Intro = () => {
     return (
         <Fragment>
             <h2>Intro</h2>
             <p>
-                The rising sun illuminates a blanket of clouds that loom over
-                the desert with a dull amber glow. Silhouetted against the sky
-                are swarms of angel like creatures, all rushing to fill gaps in
-                a gargantuan circle around the crumbled city walls. The royal
-                palace stands as a monument to the new regime, marking the
-                center of the city, and the center of a swirling black
-                thunderstorm rippled with white lightning.
+                A fleet of airships is illuminated by a backdrop of crimson
+                skies. The desert winds tear through seas of sand, sending waves
+                of dust towards the previously, proud, stone walls of Habarashi.
             </p>
             <p>
-                You all are standing on the deck of the ship as the city comes
-                into view. The banners of of your allies snap in the wind as you
-                soar towards battle; Habarashi's gold scales, New Fey's white
-                stag, the Blood Python's serpent, Mann's spiral tower, the Fey
-                Wildland's emerald tree, Sloan's scarlet dragon, and last but
-                not least, the Tunnel Snake's crest posted proudly at the helm
-                of your ship.
+                The sun sets, casting long shadows across the desert metropolis.
+                Overhead, swarms of angels, wings just as dark as these twilight
+                shadows soar, above the city ruins. They align themselves with
+                the borders of the city, standing guard as the fleet approaches,
+                their hands gripping the hilts of their blades. Within the
+                walls, mile high titans stand still, as if cast in bronze. The
+                streets are now home to shrieking beasts, skin molted and bare.
+                Missing, from this scene, are the men and dwarves that once
+                called this place their home.
             </p>
             <p>
-                From all sides, identical fleets quickly encroach on the walls
-                and the silence of the air is broken as flaming arrows begin to
-                fly. So begins the great battle of Habarashi, where blood and
+                You all are standing on the deck of your ship, as Habarashi
+                comes into view. The banners of of your allies snap in the wind
+                as you race towards battle; Habarashi's gold scales, New Fey's
+                white stag, the Blood Python's serpent, Mann's spiral tower, the
+                Fey Wildland's emerald tree, Sloan's scarlet dragon, and last
+                but not least, the Tunnel Snake's crest, posted proudly in the
+                front lines.
+            </p>
+            <p>
+                Below deck, Erika is nervously adjusting the bolts on her new
+                metal legs with Elsa's assistance. Sarah stares out of her
+                window, with a book open, pages flipping idly with the breeze.
+                It is a book she found among Diemardo's things, detailing the
+                darkest things that go bump in the night. Aamon stands at
+                Erika's door, is about to knock, but instead walks pensively
+                back to his room. He pulls out a scroll and begins to write.
+                Oddvar can be seen playing with Dennis Jr and Rodrigo in the
+                cargo hold, unaware of the violence to come. Last, but not
+                least, Eddard Stonecutter stands at the helm of the ship, his
+                fierce glowing eyes staring out at a swirling storm of
+                nightmares that looms over his former palace.
+            </p>
+            <p>
+                So will begin the great battle of Habarashi, where blood and
                 fire will fill the streets and the world's men and women fight
                 one last time for their freedom.
             </p>
